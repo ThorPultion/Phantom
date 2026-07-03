8ffd1dee4c35822a8bfac1062931ebe8a7a4c81e
ThorPultion <jeremias.hurme@gmail.com>
2026-07-03 22:08:44 +0200

    New character setup

--- Plugins/GASCore/Source/GASCore/Private/GASCoreTags.cpp
+++ Plugins/GASCore/Source/GASCore/Private/GASCoreTags.cpp
@@ -17,6 +17,7 @@ namespace GASCoreTags
 
 	// --- States ---
 	UE_DEFINE_GAMEPLAY_TAG(State_Dead, "State.Dead");
+	UE_DEFINE_GAMEPLAY_TAG(State_Weapon_Priming, "State.Weapon.Priming");
 
 	// --- Data ---
 	UE_DEFINE_GAMEPLAY_TAG(Data_Magnitude_Force, "Data.Magnitude.Force");
