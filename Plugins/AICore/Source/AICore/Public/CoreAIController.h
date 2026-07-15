108be9b9f5618bf86c85bfbc1dd9dc2f0e84faca
ThorPultion <jeremias.hurme@gmail.com>
2026-07-15 22:41:33 +0200

    Utilizing SetFocus to turn AI to target.

--- Plugins/AICore/Source/AICore/Public/CoreAIController.h
+++ Plugins/AICore/Source/AICore/Public/CoreAIController.h
@@ -90,11 +90,11 @@ protected:
 public:
 
 	/** The AIs current target (Looking, chasing, investigating, etc) */
-	UPROPERTY(Transient, BlueprintReadOnly, Category = "Memory")
+	UPROPERTY(VisibleAnywhere, Transient, BlueprintReadOnly, Category = "Memory")
 	TObjectPtr<AActor> CurrentTargetActor;
 
 	/** The state the current target is proposing that the AI should be in */
-	UPROPERTY(Transient, BlueprintReadOnly, Category = "Memory")
+	UPROPERTY(VisibleAnywhere, Transient, BlueprintReadOnly, Category = "Memory")
 	FGameplayTag CurrentTargetTag;
 	
 	void SetTarget(AActor* NewTarget);
