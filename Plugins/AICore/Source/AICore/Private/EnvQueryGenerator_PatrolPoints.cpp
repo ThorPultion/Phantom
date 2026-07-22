2057adb7141cda787c9acda09ccea06f35e9a9a4
ThorPultion <jeremias.hurme@gmail.com>
2026-07-22 23:16:39 +0200

    Code for patrol system. Not implemented in editor yet

---
+++ Plugins/AICore/Source/AICore/Private/EnvQueryGenerator_PatrolPoints.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnvQueryGenerator_PatrolPoints.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "AIPatrolSubsystem.h"
+#include "PatrolPoint.h"
+
+UEnvQueryGenerator_PatrolPoints::UEnvQueryGenerator_PatrolPoints(const FObjectInitializer& ObjectInitializer) 
+	: Super(ObjectInitializer)
+{
+	// Tell EQS this generator outputs Actors, not just raw FVectors (Locations)
+	ItemType = UEnvQueryItemType_Actor::StaticClass();
+}
+
+void UEnvQueryGenerator_PatrolPoints::GenerateItems(FEnvQueryInstance& QueryInstance) const
+{
+	const UWorld* World = QueryInstance.World;
+	if (!IsValid(World)) return;
+
+	const UAIPatrolSubsystem* PatrolSubsystem = World->GetSubsystem<UAIPatrolSubsystem>();
+	if (!IsValid(PatrolSubsystem)) return;
+	
+	TArray<AActor*> ValidPoints;
+	
+	for (APatrolPoint* Point : PatrolSubsystem->GetAllPatrolPoints())
+	{
+		// Only add points that are valid and NOT currently claimed by another AI
+		if (IsValid(Point) && !Point->IsClaimed())
+		{
+			ValidPoints.Add(Point);
+		}
+	}
+
+	// Feeding the filtered patrol points to EQS
+	QueryInstance.AddItemData<UEnvQueryItemType_Actor>(ValidPoints);
+}
+
+FText UEnvQueryGenerator_PatrolPoints::GetDescriptionTitle() const
+{
+	return FText::FromString(TEXT("Unclaimed Patrol Points"));
+}
+
+FText UEnvQueryGenerator_PatrolPoints::GetDescriptionDetails() const
+{
+	return FText::FromString(TEXT("Grabs all available patrol points from the AIPatrolSubsystem"));
+}
