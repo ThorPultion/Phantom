e688fe414da5ad7f4b1709d606d15de7915f309c
ThorPultion <jeremias.hurme@gmail.com>
2026-08-29 00:37:52 +0200

    Ability cost GE for any ability and any attribute

--- Plugins/GASCore/Source/GASCore/Private/CoreGameplayAbility.cpp
+++ Plugins/GASCore/Source/GASCore/Private/CoreGameplayAbility.cpp
@@ -5,6 +5,7 @@
 #include "AnimNotify_GameplayEvent.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "AbilitySystemComponent.h"
 
 UCoreGameplayAbility::UCoreGameplayAbility()
 {
@@ -70,4 +71,57 @@ void UCoreGameplayAbility::OnMontageCancelled()
 void UCoreGameplayAbility::OnMontageEventReceived_Implementation(FGameplayEventData Payload)
 {
 	// Child classes will override this for functionality that occurs when an AnimNotify is received
+}
+
+bool UCoreGameplayAbility::CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, OUT FGameplayTagContainer* OptionalRelevantTags) const
+{
+	// If we have no cost assigned, were free to activate
+	const UGameplayEffect* CostGE = GetCostGameplayEffect();
+	if (!CostGE) return true;
+
+	UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+	if (!IsValid(ASC)) return false;
+
+	for (const FGameplayModifierInfo& ModInfo : CostGE->Modifiers)
+	{
+		if (ModInfo.ModifierMagnitude.GetMagnitudeCalculationType() != EGameplayEffectMagnitudeCalculation::SetByCaller)
+		{
+			continue;
+		}
+
+		const FGameplayTag DataTag = ModInfo.ModifierMagnitude.GetSetByCallerFloat().DataTag;
+		const FScalableFloat* CostValue = CostData.Find(DataTag);
+
+		// If the tags dont match, go next
+		if (!CostValue) continue;
+
+		const float Cost = CostValue->GetValueAtLevel(GetAbilityLevel());
+		const float CurrentAttributeValue = ASC->GetNumericAttribute(ModInfo.Attribute);
+          
+		// Cannot afford attribute cost, prevent the commit
+		if (CurrentAttributeValue < FMath::Abs(Cost)) return false;
+	}
+
+	return true;
+}
+
+void UCoreGameplayAbility::ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+	const UGameplayEffect* CostGE = GetCostGameplayEffect();
+	if (!CostGE) return;
+	
+	// Building the spec for applying the cost
+	const FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(CostGE->GetClass(), GetAbilityLevel());
+	if (!SpecHandle.IsValid()) return;
+
+	// Injecting the cost magnitudes to the cost GE
+	for (const TPair<FGameplayTag, FScalableFloat>& CostPair : CostData)
+	{
+		const FGameplayTag& CostTag = CostPair.Key;
+		const float CurrentCost = CostPair.Value.GetValueAtLevel(GetAbilityLevel());
+              
+		SpecHandle.Data->SetSetByCallerMagnitude(CostTag, CurrentCost);
+	}
+
+	(void)ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, SpecHandle);
 }
